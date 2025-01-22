68279af51bedc3d930dfafc0f5a4fe9fc78522fa
qfilizetti <qf049323@ohio.edu>
2025-01-21 20:51:02 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Quintin Filizetti
+ *        Date: January 1, 2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
